6233dedb1ae0af2ab102a0085efe44c4500b37a1
Brandon Bordeaux <brandon.bordeaux@datastax.com>
2024-04-26 15:48:13 -0400

    Finalize the document report

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ def main():
     file = "books/frankenstein.txt"
     contents = get_file_contents(file)
     word_count = get_word_count(contents)
-    letter_count = get_letter_count(contents)
-    print(f"There are {word_count} words in this document.")
-    print(f"Letter count: {letter_count}")
+    char_count = get_char_count(contents)
+    list_of_chars = convert_letter_dict(char_count)
+    list_of_chars.sort(reverse=True, key=sort_on)
+
+    print(f"--- Begin report of {file} ---")
+    print(f"{word_count} words in this document\n")
+    for char in list_of_chars:
+        print(f"The {char["char"]} character was found {char["count"]} times")
+    print("--- End report ---")
     
 
 def get_file_contents(file):
@@ -17,17 +23,34 @@ def get_word_count(text):
     return len(words)
 
 
-def get_letter_count(text):
-    letter_count = {}
-    for letter in text:
-        lower_letter = letter.lower()
+def get_char_count(text):
+    char_count = {}
+    for char in text:
+        lowered = char.lower()
         try:
-            old_count = letter_count[lower_letter]
-            letter_count[lower_letter] = old_count + 1
+            old_count = char_count[lowered]
+            char_count[lowered] = old_count + 1
         except KeyError:
-            letter_count[lower_letter] = 1
+            char_count[lowered] = 1
 
-    return letter_count
+    return char_count
+
+
+def convert_letter_dict(dict):
+    letter_list = []
+    for key, value in dict.items():
+        if key.isalpha():
+            char_dict = {
+                "char": key,
+                "count": value
+            }
+            letter_list.append(char_dict)
+
+    return letter_list
+
+
+def sort_on(dict):
+    return dict["count"]
 
 
 main()
